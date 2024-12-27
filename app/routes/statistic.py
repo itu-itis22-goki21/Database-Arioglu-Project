0a24da37c019ff9cb33a518639ccd9ae51bca482
Yahya <yahyayahya4426@gmail.com>
2024-12-27 16:47:54 +0300

    I've seperated statistics query for count and points. And I've added a filter button to choose one of them.

--- app/routes/statistic.py
+++ app/routes/statistic.py
@@ -5,38 +5,53 @@ statistic_bp = Blueprint('statistic', __name__)
 
 @statistic_bp.route('/statistic', methods=['GET'])
 def statistic():
+    filter_type = request.args.get('filter', 'medal_points')
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
 
-    query = """
-        SELECT 
-            a.Athlete_id, 
-            a.Athlete_name, 
-            SUM(
-                CASE 
-                    WHEN m.Medal_type = 'Gold' THEN 3
-                    WHEN m.Medal_type = 'Silver' THEN 2
-                    WHEN m.Medal_type = 'Bronze' THEN 1
-                    ELSE 0
-                END
-            ) AS Medal_Points,
-            COUNT(m.Medal_id) AS Medal_Count
-        FROM 
-            athletes a 
-        JOIN 
-            medal m ON a.Athlete_id = m.Athlete_id
-        GROUP BY 
-            a.Athlete_id, 
-            a.Athlete_name
-        HAVING 
-            Medal_Points > 0
-        ORDER BY 
-            Medal_Points DESC, Medal_Count DESC;
-    """
-    
+    if filter_type == 'medal_points':
+        query = """
+            SELECT 
+                a.Athlete_id, 
+                a.Athlete_name, 
+                SUM(
+                    CASE 
+                        WHEN m.Medal_type = 'Gold' THEN 3
+                        WHEN m.Medal_type = 'Silver' THEN 2
+                        WHEN m.Medal_type = 'Bronze' THEN 1
+                        ELSE 0
+                    END
+                ) AS Medal_Points,
+                COUNT(m.Medal_id) AS Medal_Count
+            FROM 
+                athletes a 
+            JOIN 
+                medal m ON a.Athlete_id = m.Athlete_id
+            GROUP BY 
+                a.Athlete_id, 
+                a.Athlete_name
+            ORDER BY 
+                Medal_Points DESC, Medal_Count DESC;
+        """
+    elif filter_type == 'medal_count':
+        query = """
+            SELECT 
+                a.Athlete_id, 
+                a.Athlete_name, 
+                COUNT(m.Medal_id) AS Medal_Count
+            FROM 
+                athletes a 
+            JOIN 
+                medal m ON a.Athlete_id = m.Athlete_id
+            GROUP BY 
+                a.Athlete_id, 
+                a.Athlete_name
+            ORDER BY 
+                Medal_Count DESC;
+        """
     cursor.execute(query)
     statistic = cursor.fetchall()
     cursor.close()
     conn.close()
-    
-    return render_template('statistic.html', statistic=statistic)
+
+    return render_template('statistic.html', statistic=statistic, filter=filter_type)
